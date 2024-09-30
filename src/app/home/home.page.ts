03a6b5414c133cc31ca9a7bca2b1044238aadebb
Din <pudinmah69@gmail.com>
2024-09-30 19:15:47 +0700

    install swiper js

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,9 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+
+  constructor() {
+
+  }
 
 }
